7146df8615f759f69b4b725bb2201709be5c8384
cewlout <cewlout>
2005-06-13 20:31:36 +0000

    *** empty log message ***

--- CParameter.cpp
+++ CParameter.cpp
@@ -11,12 +11,7 @@ CParameter::CParameter(int argc, char **argv)
 	m_iArgc = argc;
 	m_pArgv = argv;
 
-	m_Param.apply=0;
-	m_Param.create=0;
-	m_Param.help=0;
-	m_Param.license=0;
-	m_Param.version=0;
-
+	memset(&m_Param,0,sizeof(m_Param));
 }
 
 CParameter::~CParameter()
@@ -30,33 +25,66 @@ bool CParameter::Evaluate()
 
 	RetVal=true;
 
+	// No Parameter given?
 	if(m_iArgc<=1)
 	{
 		RetVal=false;
 	}
-
-	for(i=1;i<m_iArgc;i++)
+	else
 	{
-		if(strcmp("-c",m_pArgv[i]) == 0 || strcmp("--create",m_pArgv[i]) == 0)
+		for(i=1;i<m_iArgc;i++)
 		{
-			m_Param.create+=1;
+			if(strcmp(CREATE_SHORT,m_pArgv[i]) == 0 || strcmp(CREATE_LONG,m_pArgv[i]) == 0)
+			{
+				m_Param.create+=1;
+			}
+			else if(strcmp(APPLY_SHORT,m_pArgv[i]) == 0 || strcmp(APPLY_LONG,m_pArgv[i]) == 0)
+			{
+				m_Param.apply+=1;
+			}
+			else if(strcmp(HELP_SHORT,m_pArgv[i]) == 0 || strcmp(HELP_LONG,m_pArgv[i]) == 0)
+			{
+				m_Param.help+=1;
+			}
+			else if(strcmp(FORCE_SHORT,m_pArgv[i]) == 0 || strcmp(FORCE_LONG,m_pArgv[i]) == 0)
+			{
+				m_Param.force+=1;
+			}
+			else if(strcmp(PRIMODVD_SHORT,m_pArgv[i]) == 0 || strcmp(PRIMODVD_LONG,m_pArgv[i]) == 0)
+			{
+				m_Param.primodvd+=1;
+			}
+			else if(strcmp(BLOCKCHECK_SHORT,m_pArgv[i]) == 0 || strcmp(BLOCKCHECK_LONG,m_pArgv[i]) == 0)
+			{
+				m_Param.blockcheck+=1;
+			}
+			else if(strcmp(UNDODATA_SHORT,m_pArgv[i]) == 0 || strcmp(UNDODATA_LONG,m_pArgv[i]) == 0)
+			{
+				m_Param.undodata+=1;
+			}
+			else if(strcmp(LICENSE_LONG,m_pArgv[i]) == 0)
+			{
+				m_Param.license+=1;
+			}
+			else if(strcmp(VERSION_LONG,m_pArgv[i]) == 0)
+			{
+				m_Param.version+=1;
+			}
 		}
-		else if(strcmp("-a",m_pArgv[i]) == 0 || strcmp("--apply",m_pArgv[i]) == 0)
-		{
-			m_Param.apply+=1;
-		}
-		else if(strcmp("-?",m_pArgv[i]) == 0 || strcmp("--help",m_pArgv[i]) == 0)
-		{
-			m_Param.help+=1;
-		}
-		else if(strcmp("--license",m_pArgv[i]) == 0)
-		{
-			m_Param.license+=1;
-		}
-		else if(strcmp("--version",m_pArgv[i]) == 0)
-		{
-			m_Param.version+=1;
-		}
+	}
+
+	// Any Parameter given two times?
+	if(m_Param.apply > 1
+	   || m_Param.blockcheck > 1
+		 || m_Param.create > 1
+		 || m_Param.force > 1
+		 || m_Param.help > 1
+		 || m_Param.license > 1
+		 || m_Param.primodvd > 1
+		 || m_Param.undodata > 1
+		 || m_Param.version > 1)
+	{
+		RetVal = false;
 	}
 
 	return(RetVal);
@@ -76,33 +104,33 @@ void CParameter::ShowUsage()
   printf("See www.paradogs.com \n");
   printf("\n");
   printf("Examples:\n");
-  printf("  ppftool -a -f -p=patch.ppf -o=binary.bin\n");
-  printf("  ppftool -c -p=patch.ppf -o=binary.bin -p=patched.bin -s=\"Text\" -d=id.diz\n");
-  printf("  ppftool -c -v=1 -p=patch.ppf -o=binary.bin -p=patched.bin\n");
+  printf("  ppftool %s %s %s=patch.ppf %s=binary.bin\n", APPLY_SHORT, FORCE_SHORT, PPF_SHORT, ORIGINAL_SHORT);
+  printf("  ppftool %s %s=patch.ppf %s=binary.bin %s=patched.bin %s=\"Text\" %s=id.diz\n", CREATE_SHORT, PPF_SHORT, ORIGINAL_SHORT, BINARY_SHORT, DESCRIPTION_SHORT, FILEID_SHORT);
+  printf("  ppftool %s %s=1 %s=patch.ppf %s=binary.bin %s=patched.bin\n", CREATE_SHORT, USEVERSION_SHORT, PPF_SHORT, ORIGINAL_SHORT, BINARY_SHORT);
   printf("\n");
   printf(" Main operation mode:\n");
-  printf("  -c, --create              create ppf-file\n");
-  printf("  -a, --apply               apply ppf-file\n");
+  printf("  %s, %-21s create ppf-file\n",CREATE_SHORT, CREATE_LONG);
+  printf("  %s, %-21s apply ppf-file\n", APPLY_SHORT, APPLY_LONG);
   printf("\n");
   printf(" Files modifiers:\n");
-  printf("  -b, --patched=FILE        binary-file in modified state\n");
-  printf("  -o, --original=FILE       binary-file in original state\n");
-  printf("  -p, --ppf=FILE            ppf-file (output or input)\n");
-  printf("  -d, --fileid=FILE         file_id.diz\n");
+  printf("  %s, %s=FILE        binary-file in modified state\n",BINARY_SHORT, BINARY_LONG);
+  printf("  %s, %s=FILE       binary-file in original state\n", ORIGINAL_SHORT, ORIGINAL_LONG);
+  printf("  %s, %s=FILE            ppf-file (output or input)\n", PPF_SHORT, PPF_LONG);
+  printf("  %s, %s=FILE         file_id.diz\n", FILEID_SHORT, FILEID_LONG);
   printf("\n");
   printf(" Operation modifiers: \n");
-  printf("  -f, --force               force patching if needed\n");
-  printf("  -v, --use-version=VER     use ppf-version [1/2/3] (default=3)\n");
-  printf("  -i, --primodvd-image      binary files are primodvd-files\n");
-  printf("  -l, --disable-blockcheck  disables blockcheck\n");
-  printf("  -u, --include-undo-data   includes undo-data\n");
-  printf("  -s, --description=\"TEXT\"  use \"TEXT\" as short patch-description\n");
+  printf("  %s, %-21s force patching if needed\n", FORCE_SHORT, FORCE_LONG);
+  printf("  %s, %s=VER     use ppf-version [1/2/3] (default=3)\n", USEVERSION_SHORT, USEVERSION_LONG);
+  printf("  %s, %-21s binary files are primodvd-files\n",PRIMODVD_SHORT, PRIMODVD_LONG);
+  printf("  %s, %-21s disables blockcheck\n",BLOCKCHECK_SHORT, BLOCKCHECK_LONG);
+  printf("  %s, %-21s includes undo-data\n",UNDODATA_SHORT, UNDODATA_LONG);
+  printf("  %s, %s=\"TEXT\"  use \"TEXT\" as short patch-description\n", DESCRIPTION_SHORT, DESCRIPTION_LONG);
   printf("\n");
   printf(" Other options:\n");
   printf("\n");
-  printf("  -?, --help                 Give this help list\n");
-  printf("      --license              Print license and exit\n");
-  printf("      --version              Print program versio\n");
+	printf("  %s, %-21s Give this help list\n",HELP_SHORT, HELP_LONG);
+  printf("      %-21s Print license and exit\n",LICENSE_LONG);
+  printf("      %-21s Print program version\n",VERSION_LONG);
 
 }
 
--- CParameter.h
+++ CParameter.h
@@ -10,11 +10,57 @@
 #include <stdio.h>
 #include <string.h>
 
+#define APPLY_SHORT "-a"
+#define APPLY_LONG "--apply"
+
+#define CREATE_SHORT "-c"
+#define CREATE_LONG "--create"
+
+#define HELP_SHORT "-?"
+#define HELP_LONG "--help"
+
+#define FORCE_SHORT "-f"
+#define FORCE_LONG "--force"
+
+#define PRIMODVD_SHORT "-i"
+#define PRIMODVD_LONG "--primodvd-image"
+
+#define BLOCKCHECK_SHORT "-l"
+#define BLOCKCHECK_LONG "--disable-blockcheck"
+
+#define UNDODATA_SHORT "-u"
+#define UNDODATA_LONG "--include-undo-data"
+
+#define LICENSE_LONG "--license"
+#define VERSION_LONG "--version"
+
+#define BINARY_SHORT "-b"
+#define BINARY_LONG "--patched"
+
+#define ORIGINAL_SHORT "-o"
+#define ORIGINAL_LONG "--original"
+
+#define PPF_SHORT "-p"
+#define PPF_LONG "--ppf"
+
+#define FILEID_SHORT "-d"
+#define FILEID_LONG "--fileid"
+
+#define USEVERSION_SHORT "-v"
+#define USEVERSION_LONG "--use-version"
+
+#define DESCRIPTION_SHORT "-s"
+#define DESCRIPTION_LONG "--description"
+
 struct Parameters
 {
 	int create;
 	int apply;
 	int help;
+	int force;
+	int primodvd;
+	int blockcheck;
+	int undodata;
 	int license;
 	int version;
 };
